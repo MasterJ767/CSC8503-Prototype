4edc8040726276bcd71d1a3e273f265ad8e39f00
MasterJ767 <jon.arundel@hotmail.com>
2023-01-24 18:38:32 +0000

    Create World Script

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -4,9 +4,68 @@ using UnityEngine;
 
 public class World : MonoBehaviour
 {
-    public int width;
-    public int height;
+    private const int width = 20;
+    private const int height = 20;
+    private const int cellWidth = 5;
 
     private MeshFilter meshFilter;
     private MeshRenderer meshRenderer;
+
+    private List<Vector3> vertices;
+    private List<int> triangles;
+    private List<Color> colours;
+    private Color[,] map;
+
+    private void Start() {
+        meshFilter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+        colours = new List<Color>();
+        map = new Color[width, height];
+        for (int z = 0; z < height; ++z) {
+            for(int x = 0; x < width; ++x) {
+                map[x, z] = Color.white;
+            }
+        }
+        Triangulate();
+    }
+
+    public void ChangeCell(Vector3 globalPosition, Color colour) {
+        map[Mathf.FloorToInt(globalPosition.x / cellWidth), Mathf.FloorToInt(globalPosition.z / cellWidth)] = colour;
+        Triangulate();
+    }
+
+    private void Triangulate() {
+        int i = 0;
+
+        for (int z = 0; z < height; ++z) {
+            for(int x = 0; x < width; ++x) {
+                vertices.Add(new Vector3(x * cellWidth, 0, z * cellWidth));
+                vertices.Add(new Vector3(x * cellWidth, 0, (z + 1) * cellWidth));
+                vertices.Add(new Vector3((x + 1) * cellWidth, 0, (z + 1) * cellWidth));
+                vertices.Add(new Vector3((x + 1) * cellWidth, 0, z * cellWidth));
+
+                triangles.Add(i);
+                triangles.Add(i + 1);
+                triangles.Add(i + 2);
+                triangles.Add(i);
+                triangles.Add(i + 2);
+                triangles.Add(i + 3);
+
+                colours.Add(map[x, z]);
+                colours.Add(map[x, z]);
+                colours.Add(map[x, z]);
+                colours.Add(map[x, z]);
+
+                i += 4;
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.SetVertices(vertices.ToArray());
+        mesh.SetTriangles(triangles.ToArray(), 0);
+        mesh.SetColors(colours.ToArray());
+        meshFilter.mesh = mesh;
+    }
 }
