5fcae4d90c9adc4911860bea0ec301c9a96c52a8
MasterJ767 <jon.arundel@hotmail.com>
2023-01-24 17:46:37 +0000

    Add Unity Files

---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+}
